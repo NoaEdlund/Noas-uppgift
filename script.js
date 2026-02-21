7bd841815e5fa1386405f8c1e36f7cd8978d9e19
NoaEdlund <noaedlund@gmail.com>
2026-02-21 11:33:45 +0100

    fix: Uppdaterad från button till form och från var till const

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 
 
 document.addEventListener("DOMContentLoaded", function () {
-    var items = document.querySelectorAll(".add-to-cart");
+    const items = document.querySelectorAll(".add-to-cart");
     items.forEach(function (item) {
         item.addEventListener("click", function () {
             alert("Item added to cart!");
         });
     });
 
-    var newsletterButton = document.querySelector(".newsletter .primary-button");
-    if (newsletterButton) {
-        newsletterButton.addEventListener("click", function (e) {
+    var newsletterForm = document.querySelector(".newsletter form");
+    if (newsletterForm) {
+        newsletterForm.addEventListener("submit", function (e) {
             e.preventDefault();
             alert("Thank you for subscribing!");
         });
